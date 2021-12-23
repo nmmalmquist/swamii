697243c56b30c78ee1be3b9609bafebf654b7274
nmmalmquist <nicholasmmalmquist@gmail.com>
2021-12-23 16:48:08 -0600

    temporary grid for home screen

--- swamii-client/src/screens/HomeScreen.js
+++ swamii-client/src/screens/HomeScreen.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Container } from "react-bootstrap";
+
+import styles from "../css/cssScreens/home-screen.module.css";
 
 function HomeScreen(props) {
-    return (
-        <div>
-            This is the home screen
-        </div>
-    );
+  return (
+    <Container className={styles.screen}>
+      <Container className={styles.container1}>
+        <div className={styles.title}>This weeks games</div>
+      </Container>
+      <Container className={styles.medContainer}>
+        <Container className={styles.container2}>
+          <div className={styles.title}>Standings</div>
+        </Container>
+        <Container className={styles.container3}>
+          <div className={styles.title}>Chat box</div>
+        </Container>
+      </Container>
+    </Container>
+  );
 }
 
 export default HomeScreen;
