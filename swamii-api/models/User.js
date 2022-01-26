1afe1c2178d31a6352561cc27329a53b514e92eb
nmmalmquist <nicholasmmalmquist@gmail.com>
2022-01-25 19:56:35 -0600

    bcrypt

--- swamii-api/models/User.js
+++ swamii-api/models/User.js
@@ -29,7 +29,7 @@ const userSchema = new mongoose.Schema({
     },
     avatarURL: {
         type: String,
-        required: true,
+        required: false,
     },
     dateCreated: {
         type: Date,
--- swamii-api/routes/auth.js
+++ swamii-api/routes/auth.js
@@ -3,6 +3,7 @@ const User = require("../models/User");
 const Admin = require("../models/Admin");
 const { registerValidation, loginValidation } = require("../validation");
 const argon = require("argon2");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // router is an instance of the express router.
@@ -16,23 +17,42 @@ const router = express.Router();
 router.post("/user/register", async (req, res) => {
   //Check Validation before adding user
   const { error } = registerValidation(req.body);
-  if (error) return res.send({errorState: 1, message: error.details[0].message});
+  if (error)
+    return res.send({ errorState: 1, message: error.details[0].message });
 
   //Check if user already in Database
-  const emailExists = await User.findOne({ email: req.body.email.toLowerCase() });
-  const usernameExists = await User.findOne({ username: req.body.username.toLowerCase() });
-  if (emailExists) return res.send({errorState: 3, message: "Email already exists in database"});
-  if (usernameExists) return res.send({errorState: 3, message: "Username already exists in database"});
+  const emailExists = await User.findOne({
+    email: req.body.email.toLowerCase(),
+  });
+  const usernameExists = await User.findOne({
+    username: req.body.username.toLowerCase(),
+  });
+  if (emailExists)
+    return res.send({
+      errorState: 3,
+      message: "Email already exists in database",
+    });
+  if (usernameExists)
+    return res.send({
+      errorState: 3,
+      message: "Username already exists in database",
+    });
 
   //Hash password
-  let hashPassword = req.body.pasword;
+  let hashPassword = req.body.password;
   try {
-    hashPassword = await argon.hash(req.body.password);
+    const hash = bcrypt.hashSync(req.body.password, 10);
+    hashPassword = hash;
   } catch (error) {
     console.log("Could not hash password : " + error);
   }
+
+  
   if (hashPassword == req.body.password)
-    return res.send({errorState: 4, message: "Could not hash password. User Not created"});
+    return res.send({
+      errorState: 4,
+      message: "Could not hash password. User Not created",
+    });
 
   //create new user object
   const user = new User({
@@ -43,7 +63,7 @@ router.post("/user/register", async (req, res) => {
     password: hashPassword,
     initialBalance: 1000,
     currentBalance: 1000,
-    deleted: false
+    deleted: false,
   });
 
   //save new user to database
@@ -51,35 +71,40 @@ router.post("/user/register", async (req, res) => {
     const savedUser = await user.save();
     res.send({ user: user._id });
   } catch (error) {
-    res.send({errorState: 400, message: "Could not save user to DB"})
+    res.send({ errorState: 400, message: "Could not save user to DB" });
     console.log("ERROR : " + error);
   }
 });
-
 
 ///////////////////////////////////////
 //LOGIN A USER
 ////////////////////////////////////////
 
 router.post("/user/login", async (req, res) => {
-  
   //Check Validation before adding user
   const { error } = loginValidation(req.body);
-  if (error) return res.send({errorState: 1, message: error.details[0].message})
+  if (error)
+    return res.send({ errorState: 1, message: error.details[0].message });
 
   //Check if user username exists
-  const user = await User.findOne({ username: req.body.username.toLowerCase() });
-  console.log(user)
-  if (!user) return res.send({errorState: 0, message: "Username is invalid"});
+  const user = await User.findOne({
+    username: req.body.username.toLowerCase(),
+  });
+  console.log(user);
+  if (!user) return res.send({ errorState: 0, message: "Username is invalid" });
 
   //Check if password is correct
-  const validPassword = await argon.verify(user.password, req.body.password);
+  const validPassword = bcrypt.compareSync(req.body.password, user.password);
+ 
 
   if (!validPassword)
-    return res.send({errorState: 0, message: "Password is invalid"});
+    return res.send({ errorState: 0, message: "Password is invalid" });
 
   //If all validation checks out, then Create web token and send to front-end
-  const token = jwt.sign({ _id: user._id, username: user.username, avatarURL: user.avatarURL }, process.env.TOKEN_SECRET);
+  const token = jwt.sign(
+    { _id: user._id, username: user.username, avatarURL: user.avatarURL },
+    process.env.TOKEN_SECRET
+  );
   res.header("auth-token", token).send(token);
 });
 
@@ -87,67 +112,82 @@ router.post("/user/login", async (req, res) => {
 //REGISTER A NEW ADMIN
 ////////////////////////////////////////
 router.post("/admin/register", async (req, res) => {
-    //Check Validation before adding admin
-    const { error } = registerValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-  
-    //Check if admin already in Database
-    const emailExists = await Admin.findOne({ email: req.body.email.toLowerCase() });
-    const usernameExists = await Admin.findOne({ username: req.body.username.toLowerCase()  });
-    if (emailExists) return res.send({errorState: 3, message: "Email already exists in database"});
-    if (usernameExists) return res.send({errorState: 3, message: "Username already exists in database"});
-  
-    //Hash password
-    let hashPassword = req.body.pasword;
-    try {
-      hashPassword = await argon.hash(req.body.password);
-    } catch (error) {
-      console.log("Could not hash password : " + error);
-    }
-    if (hashPassword == req.body.password)
-      return res.send({errorState: 4, message: "Could not hash password. User Not created"});
-  
-    //create new Admin object
-    const admin = new Admin({
-      firstName: req.body.firstName.toLowerCase() ,
-      lastName: req.body.lastName.toLowerCase() ,
-      username: req.body.username.toLowerCase() ,
-      email: req.body.email.toLowerCase() ,
-      password: hashPassword,
+  //Check Validation before adding admin
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //Check if admin already in Database
+  const emailExists = await Admin.findOne({
+    email: req.body.email.toLowerCase(),
+  });
+  const usernameExists = await Admin.findOne({
+    username: req.body.username.toLowerCase(),
+  });
+  if (emailExists)
+    return res.send({
+      errorState: 3,
+      message: "Email already exists in database",
     });
-  
-    //save new admin to database
-    try {
-      const savedAdmin = await admin.save();
-      res.send({ admin: admin._id });
-    } catch (error) {
-      res.send({errorState: 400, message: "Could not save user to DB"})
-      console.log("ERROR : " + error);
-    }
+  if (usernameExists)
+    return res.send({
+      errorState: 3,
+      message: "Username already exists in database",
+    });
+
+  //Hash password
+  let hashPassword = req.body.pasword;
+  try {
+    hashPassword = await argon.hash(req.body.password);
+  } catch (error) {
+    console.log("Could not hash password : " + error);
+  }
+  if (hashPassword == req.body.password)
+    return res.send({
+      errorState: 4,
+      message: "Could not hash password. User Not created",
+    });
+
+  //create new Admin object
+  const admin = new Admin({
+    firstName: req.body.firstName.toLowerCase(),
+    lastName: req.body.lastName.toLowerCase(),
+    username: req.body.username.toLowerCase(),
+    email: req.body.email.toLowerCase(),
+    password: hashPassword,
   });
 
-  ///////////////////////////////////////
+  //save new admin to database
+  try {
+    const savedAdmin = await admin.save();
+    res.send({ admin: admin._id });
+  } catch (error) {
+    res.send({ errorState: 400, message: "Could not save user to DB" });
+    console.log("ERROR : " + error);
+  }
+});
+
+///////////////////////////////////////
 //LOGIN An Admin
 ////////////////////////////////////////
 
 router.post("/admin/login", async (req, res) => {
-    //Check Validation before adding admin
-    const { error } = loginValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-  
-    //Check if admin username exists
-    const admin = await Admin.findOne({ username: req.body.username });
-    if (!admin) return res.status(400).send("Username or Password is not valid");
-  
-    //Check if password is correct
-    const validPassword = await argon.verify(admin.password, req.body.password);
-    if (!validPassword)
-      return res.status(400).send("Email or Password is invalid");
-  
-    //If all validation checks out, then Create web token and send to front-end
-    const token = jwt.sign({ _id: admin._id }, process.env.TOKEN_SECRET);
-    res.header("auth-token", token).send(token);
-    console.log("admin logged in")
-  });
+  //Check Validation before adding admin
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //Check if admin username exists
+  const admin = await Admin.findOne({ username: req.body.username });
+  if (!admin) return res.status(400).send("Username or Password is not valid");
+
+  //Check if password is correct
+  const validPassword = await argon.verify(admin.password, req.body.password);
+  if (!validPassword)
+    return res.status(400).send("Email or Password is invalid");
+
+  //If all validation checks out, then Create web token and send to front-end
+  const token = jwt.sign({ _id: admin._id }, process.env.TOKEN_SECRET);
+  res.header("auth-token", token).send(token);
+  console.log("admin logged in");
+});
 
 module.exports = router;
